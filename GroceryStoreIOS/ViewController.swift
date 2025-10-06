d65aa392de852763a5f8ad6337b18d3575f085d6
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2025-10-06 17:26:58 +0300

    Initial Commit

---
+++ GroceryStoreIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroceryStoreIOS
+//
+//  Created by Ömerfaruk Saribal on 6.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
